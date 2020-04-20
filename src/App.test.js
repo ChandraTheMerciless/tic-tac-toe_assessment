cc4c1bfdbd738c13fb5f1d22ba7310fad430c232
Chandra Asar <chandra_asar@anfcorp.com>
2020-04-20 13:16:29 -0400

    removed some extra code and added first simple test in mocha chai with enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,21 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import ReactDOM from 'react-dom';
+import Game from './components/Game/Game';
+// import { render } from '@testing-library/react';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+import App from './App';
+import { mount, shallow } from 'enzyme';
+import { expect } from 'chai';
+
+describe('<App />', () => {
+  it('contains a <Game /> component', () => {
+    const wrapper = mount(<App/>);
+    expect(wrapper.find(Game)).to.have.length(1);
+  });
 });
+// NOTE: come back to this later
+// test('renders learn react link', () => {
+//   const { getByText } = render(<App />);
+//   const linkElement = getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
