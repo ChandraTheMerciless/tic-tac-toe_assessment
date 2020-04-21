019f0bc8ee13516cf34ecb292ff30d0587d353f9
Chandra Asar <chandra_asar@anfcorp.com>
2020-04-21 10:48:53 -0400

    got test to check for 9 squares in board component passing

--- src/components/Board/board.component.jsx
+++ src/components/Board/board.component.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
+import Square from '../Square/square.component';
 import './board.styles.scss';
 
 class Board extends React.Component {
+  // hard coded because the number of square in tic tac toe won't change
+  squareAmount = 9;
+
   constructor() {
     super();
 
     this.state = {
-
+      squares: []
     };
+  }
+
+  componentDidMount () {
+    this.setState({squares: Array(this.squareAmount).fill({})});
   }
 
   render() {
     return (
-      <div>Board TEST</div>
+      <div>
+        {
+          this.state.squares.map((square, id) => {
+            return <Square key={id} />
+          })
+        }
+      </div>
     );
   }
 }
---
+++ src/components/Square/square.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './square.styles.scss';
+
+const Square = () => {
+  return (
+    <div>Square Test</div>
+  )
+}
+
+export default Square;
