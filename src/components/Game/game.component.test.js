31d25d9ef403990b8aff8cd179ec61d18a1bd49f
Chandra Asar <chandra_asar@anfcorp.com>
2020-04-22 13:01:09 -0400

    committing 8 breaking tests to test winning logic. Committing 8 tests instead of 1 because one function will fix all tests at once

--- src/components/Game/game.component.test.js
+++ src/components/Game/game.component.test.js
@@ -46,10 +46,100 @@ describe('<Game />', () => {
     const wrapper = mount(<Game />);
     wrapper.find('button').first().simulate('click');
     expect(wrapper.state('nextPlayer')).to.equal('O');
-  })
+  });
 
   it('sets the winner state to an empty string if no squares have been clicked', () => {
     const wrapper = mount(<Game />);
     expect(wrapper.state('winner')).to.equal('');
-  })
+  });
+
+  it('sets the winner state to an X if a winning condition for X on first row is true', () => {
+    const wrapper = mount(<Game />);
+    wrapper.find('button').at(0).simulate('click');
+    wrapper.find('button').at(4).simulate('click');
+    wrapper.find('button').at(1).simulate('click');
+    wrapper.find('button').at(5).simulate('click');
+    wrapper.find('button').at(2).simulate('click');
+
+    expect(wrapper.state('winner')).to.equal('X');
+  });
+
+  it('sets the winner state to an O if a winning condition for O on second row is true', () => {
+    const wrapper = mount(<Game />);
+    wrapper.find('button').at(0).simulate('click');
+    wrapper.find('button').at(3).simulate('click');
+    wrapper.find('button').at(6).simulate('click');
+    wrapper.find('button').at(4).simulate('click');
+    wrapper.find('button').at(2).simulate('click');
+    wrapper.find('button').at(5).simulate('click');
+
+    expect(wrapper.state('winner')).to.equal('O');
+  });
+
+  it('sets the winner state to an X if a winning condition for X on last row is true', () => {
+    const wrapper = mount(<Game />);
+    wrapper.find('button').at(6).simulate('click');
+    wrapper.find('button').at(0).simulate('click');
+    wrapper.find('button').at(7).simulate('click');
+    wrapper.find('button').at(4).simulate('click');
+    wrapper.find('button').at(8).simulate('click');
+
+    expect(wrapper.state('winner')).to.equal('X');
+  });
+
+  it('sets the winner state to an X if a winning condition for X on first column is true', () => {
+    const wrapper = mount(<Game />);
+    wrapper.find('button').at(0).simulate('click');
+    wrapper.find('button').at(1).simulate('click');
+    wrapper.find('button').at(3).simulate('click');
+    wrapper.find('button').at(2).simulate('click');
+    wrapper.find('button').at(6).simulate('click');
+
+    expect(wrapper.state('winner')).to.equal('X');
+  });
+
+
+  it('sets the winner state to an X if a winning condition for X on second column is true', () => {
+    const wrapper = mount(<Game />);
+    wrapper.find('button').at(1).simulate('click');
+    wrapper.find('button').at(0).simulate('click');
+    wrapper.find('button').at(4).simulate('click');
+    wrapper.find('button').at(2).simulate('click');
+    wrapper.find('button').at(7).simulate('click');
+
+    expect(wrapper.state('winner')).to.equal('X');
+  });
+
+  it('sets the winner state to an X if a winning condition for X on third column is true', () => {
+    const wrapper = mount(<Game />);
+    wrapper.find('button').at(2).simulate('click');
+    wrapper.find('button').at(0).simulate('click');
+    wrapper.find('button').at(5).simulate('click');
+    wrapper.find('button').at(1).simulate('click');
+    wrapper.find('button').at(8).simulate('click');
+
+    expect(wrapper.state('winner')).to.equal('X');
+  });
+
+  it('sets the winner state to an X if a winning condition for X on backward diagonal is true', () => {
+    const wrapper = mount(<Game />);
+    wrapper.find('button').at(0).simulate('click');
+    wrapper.find('button').at(3).simulate('click');
+    wrapper.find('button').at(4).simulate('click');
+    wrapper.find('button').at(1).simulate('click');
+    wrapper.find('button').at(7).simulate('click');
+
+    expect(wrapper.state('winner')).to.equal('X');
+  });
+
+  it('sets the winner state to an X if a winning condition for X on forward diagonal is true', () => {
+    const wrapper = mount(<Game />);
+    wrapper.find('button').at(2).simulate('click');
+    wrapper.find('button').at(3).simulate('click');
+    wrapper.find('button').at(4).simulate('click');
+    wrapper.find('button').at(1).simulate('click');
+    wrapper.find('button').at(6).simulate('click');
+
+    expect(wrapper.state('winner')).to.equal('X');
+  });
 });
