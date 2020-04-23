a291c3625565e39e71ffbfa5529d4b37eff83103
Chandra Asar <chandra_asar@anfcorp.com>
2020-04-23 12:21:49 -0400

    added a couple of breaking tests for custom button attributes

---
+++ src/components/CustomButton/custom-button.component.test.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import CustomButton from '../CustomButton/custom-button.component';
+import Adapter from 'enzyme-adapter-react-16';
+import { configure, mount, shallow } from 'enzyme';
+import { expect } from 'chai';
+
+configure({ adapter: new Adapter() });
+
+describe('<CustomButtom />', () => {
+  it('sets text props', () => {
+    const wrapper = mount(<CustomButton text='foo'/>);
+    const buttonEl = wrapper.find('button').first();
+    expect(buttonEl.text()).to.equal('foo');
+  });
+  it('sets disabled props', () => {
+    const wrapper = mount(<CustomButton isDisabled='true'/>);
+    const buttonEl = wrapper.find('button').first();
+    expect(buttonEl.attribs['disabled']).to.equal(true);
+  });
+});
