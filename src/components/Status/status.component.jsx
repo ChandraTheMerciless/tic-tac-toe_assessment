c883126a0753f58ca56245b4f54a4509ad4152d2
Chandra Asar <chandra_asar@anfcorp.com>
2020-04-24 13:56:04 -0400

    made tests pass by adding new functionality

--- src/components/Status/status.component.jsx
+++ src/components/Status/status.component.jsx
@@ -2,16 +2,26 @@ import React from 'react';
 import './status.styles.scss';
 
 const Status = (props) => {
-  
+  const getMissingSquaresText = () => {
+    return props.squaresText.find(text => !text) !== undefined;
+  }
+
+  const getTextIfNoWinner = () => {
+    debugger
+    let statusMessage = getMissingSquaresText() ?
+      `The next player is ${props.player}`
+    :
+      `The game is tied! Press Restart Game to try again!`;
+    return statusMessage;
+  }
+
   return (
-    <div
-      className='status-container'
-    >
+    <div className='status-container'>
       {
         !!props.winner ? 
           `The winner is ${props.winner}!`
         :
-          `The next player is ${props.player}`
+          getTextIfNoWinner()
       }
     </div>
   );
